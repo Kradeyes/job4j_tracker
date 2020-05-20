eec6dcb3046ebb1357c65aecf83e783c37fcce11
Kradeyes <yanin.r.a@mail.ru>
2020-05-20 21:45:13 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора.[34117#275266]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -13,8 +13,9 @@ public class ValidateInputTest {
         ByteArrayOutputStream mem = new ByteArrayOutputStream();
         PrintStream out = System.out;
         System.setOut(new PrintStream(mem));
-        String[] data = {"one", "1"};
-        ValidateInput input = new ValidateStubInput(data);
+        ValidateInput input = new ValidateInput(
+                new StubInput(new String[] {"one", "1"})
+        );
         input.askInt("Enter");
         assertThat(
                 mem.toString(),
@@ -23,13 +24,16 @@ public class ValidateInputTest {
         System.setOut(out);
     }
 
+
+
     @Test
     public void whenMaxInput() {
         ByteArrayOutputStream mem = new ByteArrayOutputStream();
         PrintStream out = System.out;
         System.setOut(new PrintStream(mem));
-        String[] data = {"7", "4"};
-        ValidateInput input = new ValidateStubInput(data);
+        ValidateInput input = new ValidateInput(
+                new StubInput(new String[] {"7", "4"})
+        );
         input.askInt("Enter", 5);
         assertThat(
                 mem.toString(),
