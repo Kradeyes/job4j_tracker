b5030c2b2cb34ccc0e497f26b0d6ef8fdc30bd36
Kradeyes <yanin.r.a@mail.ru>
2020-06-23 15:30:48 +0300

    2.2. Лямбда блок[249208#275089]

--- src/main/java/ru/job4j/lambda/AttachmentSort.java
+++ src/main/java/ru/job4j/lambda/AttachmentSort.java
@@ -11,20 +11,16 @@ public class AttachmentSort {
                 new Attachment("image 2", 34),
                 new Attachment("image 3", 13)
         );
-        Comparator<Attachment> comparator = new Comparator<>() {
-            @Override
-            public int compare(Attachment o1, Attachment o2) {
-                return o1.getSize() - o2.getSize();
-            }
-        };
+        Comparator<Attachment> comparator = (o1, o2) -> o1.getSize() - o2.getSize();
+
         attachments.sort(comparator);
         System.out.println(attachments);
 
-        Comparator<Attachment> nameComp = new Comparator<>() {
-            @Override
-            public int compare(Attachment o1, Attachment o2) {
-                return o1.getName().compareTo(o2.getName());
-            }
+        Comparator<Attachment> nameComp = (o1, o2) -> o1.getName().compareTo(o2.getName());
+
+        Comparator<String> cmpDescSize = (left, right) -> {
+            System.out.println("Compare -" + right.length() + " and" + left.length());
+            return right.length() - left.length();
         };
+        }
     }
-}
