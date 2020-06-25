40a9e64effb02e8e4c249ff3f2cbfcd2586a734f
Kradeyes <yanin.r.a@mail.ru>
2020-06-25 15:58:46 +0300

    Контрольные вопросы.[10097#275221]

--- src/main/java/ru/job4j/collection/FreezeStr.java
+++ src/main/java/ru/job4j/collection/FreezeStr.java
@@ -1,16 +1,39 @@
 package ru.job4j.collection;
 
+import javax.swing.*;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FreezeStr {
     public static boolean eq(String left, String right) {
-        char[] firstWord = left.toCharArray();
-        char[] secondWord = right.toCharArray();
-        Arrays.sort(firstWord);
-        Arrays.sort(secondWord);
-        if (firstWord.length == secondWord.length) {
-            return Arrays.equals(firstWord, secondWord);
+        if (left.length() != right.length()) {
+            return false;
         }
-        return false;
+        char[] firstWord = left.toLowerCase().toCharArray();
+        char[] secondWord = right.toLowerCase().toCharArray();
+
+        char[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                          'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (char l : letters) {
+            map.put(l, 0);
+        }
+
+        for (int i = 0; i < firstWord.length; i++) {
+            Integer countLeft = map.get(firstWord[i]);
+            map.put(firstWord[i], ++countLeft);
+            Integer countRight = map.get(secondWord[i]);
+            map.put(secondWord[i], --countRight);
+        }
+
+        for (Integer mapCheck : map.values()) {
+            if (mapCheck != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
