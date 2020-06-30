236a83d9bb55ce252ca58901625557c4d89ee6c1
Kradeyes <yanin.r.a@mail.ru>
2020-06-30 18:17:09 +0300

    1. Фильтрация учеников.[110058#275096] #2

--- src/main/java/ru/job4j/streams/School.java
+++ src/main/java/ru/job4j/streams/School.java
@@ -6,8 +6,7 @@ import java.util.stream.Collectors;
 
 public class School {
     public static List<Student> collect(List<Student> students, Predicate<Student> predict) {
-     List<Student> allStudents = new ArrayList<>(students);
-        return allStudents.stream()
+        return students.stream()
                           .filter(predict)
                           .collect(Collectors.toList());
     }
