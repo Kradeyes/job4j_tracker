3bd2477d7ab59a1f3ab437c902f4fe6ce533a062
Kradeyes <yanin.r.a@mail.ru>
2020-06-30 16:27:33 +0300

    4. Преобразование List в Map.[110226#275099]

--- src/main/java/ru/job4j/streams/School.java
+++ src/main/java/ru/job4j/streams/School.java
@@ -11,4 +11,17 @@ public class School {
                           .filter(predict)
                           .collect(Collectors.toList());
     }
+
+    public static void main(String[] args) {
+        List<Student> studentsForMap = List.of(
+                new Student("Petrov", 90),
+                new Student("Ivanov", 60),
+                new Student("Sidorov", 20),
+                new Student("Ivanova", 70)
+        );
+        Map<String, Student> map = studentsForMap.stream()
+                                  .distinct()
+                                  .collect(Collectors
+                                          .toMap(Student::getSurname, student -> student));
+    }
 }
