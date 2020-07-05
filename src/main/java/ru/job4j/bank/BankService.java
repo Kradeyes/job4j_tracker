8542a089f85f458e1c553f579f7f374da947fc11
Kradeyes <yanin.r.a@mail.ru>
2020-07-05 21:59:39 +0300

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API.[24260#275094]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,47 @@
+package ru.job4j.bank;
+
+import java.util.*;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<>());
+
+    }
+
+    public void addAccount(String passport, Account account) {
+        Optional<User> user = findByPassport(passport);
+        if (user.isPresent()) {
+            if (!users.get(user.get()).contains(account)) {
+                users.get(user.get()).add(account);
+            }
+        }
+    }
+
+    public Optional<User> findByPassport(String passport) {
+        return users.keySet().stream()
+                             .filter(user -> user.getPassport().equals(passport))
+                             .findFirst();
+    }
+
+    public Optional<Account> findByRequisite(String passport, String requisite) {
+        Optional<User> user = findByPassport(passport);
+        return user.flatMap(value -> users.get(value).stream()
+                                    .filter(account -> account.getRequisite().equals(requisite))
+                                    .findFirst());
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite, String destPassport,
+                                 String destRequisite, double amount) {
+        boolean rsl = false;
+        Optional<Account> src = findByRequisite(srcPassport, srcRequisite);
+        Optional<Account> dest = findByRequisite(destPassport, destRequisite);
+        if (src.isPresent() && dest.isPresent() && src.get().getBalance() >= amount) {
+            src.get().setBalance(src.get().getBalance() - amount);
+            dest.get().setBalance(dest.get().getBalance() + amount);
+            rsl = true;
+        }
+        return rsl;
+    }
+}
