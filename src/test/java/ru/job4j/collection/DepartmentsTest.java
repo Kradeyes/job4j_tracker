9274d1977312355def431deb30a210f675c50e78
Kradeyes <yanin.r.a@mail.ru>
2020-07-08 13:06:22 +0300

    1. Collection API Улучшения[70623#275105] #2

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -1,8 +1,6 @@
 package ru.job4j.collection;
 
 import org.junit.Test;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -10,8 +8,8 @@ import static org.junit.Assert.assertThat;
 public class DepartmentsTest {
     @Test
     public void whenMissed() {
-        List<String> input = Arrays.asList("k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         Departments.sortAsc(result);
         assertThat(result, is(expect));
@@ -19,8 +17,8 @@ public class DepartmentsTest {
 
     @Test
     public void whenNonChange() {
-        List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         Departments.sortAsc(result);
         assertThat(result, is(expect));
@@ -28,8 +26,8 @@ public class DepartmentsTest {
 
     @Test
     public void whenDescOrder() {
-        List<String> input = Arrays.asList("k1", "k1/sk1", "k2");
-        List<String> expect = Arrays.asList("k2", "k1", "k1/sk1");
+        List<String> input = List.of("k1", "k1/sk1", "k2");
+        List<String> expect = List.of("k2", "k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         Departments.sortDesc(result);
         assertThat(result, is(expect));
